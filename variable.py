f9d3e10c0985d00b8bb65084c7a9e992896f5758
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-06-29 11:34:11 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,66 @@
+# a=1
+# print(a)
+# a=1
+# print(a) #1
+# b=12
+# print(b) #12
+# _=2
+# print(_) #
+# a_v=12
+# print(a_v) #12
+# a_b=12
+# print(a_b) #12
+# a_7=4
+# print(a_7)
+# _a2=4
+# print(_a2) #4
+# _a_=4
+# print(_a_) #4
+# a22=2
+# print(a22) #2
+# aa2=7
+# print(aa2)
+# aaa=4
+# print(aaa) #4
+# _u_=3
+# print(_u_) #3
+# a_a=5
+# print(a_a)
+# _33=4
+# print(_33)
+# __u=9
+# print(__u) #9
+# ___=7
+# print(___) #7
+# aaa=11
+# print(aaa) #11
+# _22=4
+# print(_22) #4
+# _aa=33
+# print(_aa) #33
+# uuu=4
+# print(uuu) #4
+# _44=5
+# print(_44) #5
+# qqqqq=7
+# print(qqqqq) #7
+# _22222=7
+# print(_22222) #7
+# x_yz=11
+# print(x_yz) #11
+# r6u=55
+# print(r6u) #55
+#  w=6i
+#   print(w)
+# if 6<7:
+#  print('true')
+# else:
+# print('false')
+# if 6<7:
+    # print('true')
+# else:
+    # print('false')
+# print('hello')
+# print('_34',isidentifier())
+# print('_7v',isidentifier())
+# print('ab2',isidentifier())
